06873b484066614c64162ed664659b6f47ce3a5c
mtamseka <tammukul@gmail.com>
2015-12-17 15:35:21 -0800

    Fixed some stuff

--- Assets/Scripts/ChatSetup.cs
+++ Assets/Scripts/ChatSetup.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 using ExitGames.Client.Photon.Chat;
 using UnityEngine.UI;
 
@@ -11,18 +12,31 @@ public class ChatSetup : MonoBehaviour,IChatClientListener {
 	public Text buttonText;
 	public GameObject chatBlockObject;
 	public GameObject scrollableChat;
-	int numberOfMessages = 0;
 	float gapSize = 42.0f;
 	List<GameObject> chatBlocks;
 	public Sprite[] sprites;
 	public GameObject connectButtons;
 	public Text statusText;
-
+	public GameObject chatBackground;
+	Vector3 originalPosScrollPanel,originalPosChatBackground;
+	public GameObject scrollPanel;
+	public GameObject hiddenChatBoxButton;
+	
+	public int maximumNumberOfMsgs = 100;
 	// Use this for initialization
 	void Start () {
 		
 		chatBlocks = new List<GameObject>();
+		originalPosScrollPanel = scrollPanel.GetComponent<RectTransform>().anchoredPosition3D;
+		originalPosChatBackground = chatBackground.GetComponent<RectTransform>().anchoredPosition3D;
 		
+		Vector3 movePositionTo = chatBackground.GetComponent<RectTransform>().anchoredPosition3D;
+		movePositionTo.x = -200.0f;
+		chatBackground.GetComponent<RectTransform>().anchoredPosition3D = movePositionTo;
+		movePositionTo = originalPosScrollPanel;
+		movePositionTo.y = -159.0f;
+		scrollPanel.GetComponent<RectTransform>().anchoredPosition3D = movePositionTo;
+		scrollPanel.GetComponent<ScrollRect>().vertical = false;
 	}
 	
 	// Update is called once per frame
@@ -65,9 +79,105 @@ public class ChatSetup : MonoBehaviour,IChatClientListener {
 		chatText.interactable = false;
 	}
 	
+	IEnumerator ShowChat(string[] senders, object[] messages)
+	{
+		for ( int i = 0; i < senders.Length; i++ )
+		{
+			StartCoroutine(PopText(senders[i],messages[i].ToString()));
+			yield return new WaitForSeconds(0.6f);
+		}
+	}
+	IEnumerator PopText(string playerName,string message)
+	{
+		if(chatBlocks.Count > 0)
+		{
+			for(int i=0;i<chatBlocks.Count;i++)
+			{
+				Vector3 chatBlockPosition = chatBlocks[i].GetComponent<RectTransform>().anchoredPosition3D;
+				chatBlockPosition.x = 0.0f;
+				chatBlockPosition.y -= gapSize;
+				LeanTween.move(chatBlocks[i].GetComponent<RectTransform>(),chatBlockPosition,0.5f);
+			}
+		}
+		// Wait for the leantween animation to complete..
+		yield return new WaitForSeconds(0.5f);
+		
+		//Create Chat Block if the maximum number of msgs is not met otherwise resuse the oldest chat block.
+		GameObject tempChatBlock;
+		if(chatBlocks.Count != maximumNumberOfMsgs)
+		{
+			tempChatBlock = Instantiate(chatBlockObject,Vector3.zero,Quaternion.identity) as GameObject;
+		}
+		else
+		{
+			tempChatBlock = chatBlocks[0];
+			chatBlocks.RemoveAt(0);
+		}
+		ChatBlock cb = tempChatBlock.GetComponent<ChatBlock>();
+		cb.setAlphaAsZero();
+
+		if(playerName == "BMoney")
+		{
+			cb.setImage(sprites[0]);
+		}
+		else if(playerName == "Reyles")
+		{
+			cb.setImage(sprites[1]);
+		}
+		else if(playerName == "SaintKW")
+		{
+			cb.setImage(sprites[2]);
+		}
+		
+		cb.setText(message);
+		cb.fadeInBlock();
+		
+		//Reset
+		tempChatBlock.transform.SetParent(scrollableChat.transform);
+		tempChatBlock.GetComponent<RectTransform>().localScale = Vector3.one;
+		tempChatBlock.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
+
+		//Scrollable Chat Size
+		if(chatBlocks.Count != maximumNumberOfMsgs)
+		{
+			Vector2 tempSize = scrollableChat.GetComponent<RectTransform>().sizeDelta;
+			tempSize.y += gapSize;
+			scrollableChat.GetComponent<RectTransform>().sizeDelta = tempSize;
+		}
+		
+		//Add block to the list
+		chatBlocks.Add(tempChatBlock);
+		
+		chatText.interactable = true;
+	}
+	
+	public void openChat()
+	{
+		hiddenChatBoxButton.SetActive(false);
+		scrollPanel.GetComponent<ScrollRect>().vertical = true;
+		LeanTween.move(scrollPanel.GetComponent<RectTransform>(),originalPosScrollPanel,0.5f);
+		LeanTween.move(chatBackground.GetComponent<RectTransform>(),originalPosChatBackground,0.5f);
+	}
+	
+	public void closeChat()
+	{
+		hiddenChatBoxButton.SetActive(true);
+		
+		Vector3 movePositionTo = chatBackground.GetComponent<RectTransform>().anchoredPosition3D;
+		movePositionTo.x = -200.0f;
+		LeanTween.move(chatBackground.GetComponent<RectTransform>(),movePositionTo,1.0f);
+		
+		movePositionTo = originalPosScrollPanel;
+		movePositionTo.y = -159.0f;
+		LeanTween.move(scrollPanel.GetComponent<RectTransform>(),movePositionTo,1.0f);
+	}
+
+
+
+	// Listener Functions for the chat module
 	public void OnConnected()
     {
-		chatClient.Subscribe(new string[] {"Global"},3);
+		chatClient.Subscribe(new string[] {"Global"},10);
     }
 	
 	public void DebugReturn(ExitGames.Client.Photon.DebugLevel level, string message)
@@ -100,10 +210,7 @@ public class ChatSetup : MonoBehaviour,IChatClientListener {
 	}
 	public void OnGetMessages(string channelName, string[] senders, object[] messages)
     {
-		for ( int i = 0; i < senders.Length; i++ )
-		{
-			PopTextInChat(senders[i],messages[i].ToString());
-		}
+		StartCoroutine(ShowChat(senders,messages));
 	}
 	
 	public void OnPrivateMessage(string sender, object message, string channelName)
@@ -125,61 +232,5 @@ public class ChatSetup : MonoBehaviour,IChatClientListener {
 		
 	}
 	
-	void PopTextInChat(string playerName,string message)
-	{
-		
-		// Move the old Chat down to make way for the new one.
-		if(chatBlocks.Count > 0)
-		{
-			for(int i=0;i<chatBlocks.Count;i++)
-			{
-				Vector3 chatBlockPosition = chatBlocks[i].GetComponent<RectTransform>().anchoredPosition3D;
-				chatBlockPosition.x = 0.0f;
-				chatBlockPosition.y -= gapSize;
-				LeanTween.move(chatBlocks[i].GetComponent<RectTransform>(),chatBlockPosition,0.5f);
-			}
-		}
-		
-		//Create Chat Block
-		GameObject tempChatBlock = Instantiate(chatBlockObject,Vector3.zero,Quaternion.identity) as GameObject;
-		ChatBlock cb = tempChatBlock.GetComponent<ChatBlock>();
-		cb.setAlphaAsZero();
-		
-		//I got slightly confused with Player names... 
-		//Since I saw hardcoded names on images...so for testing purposes..
-		//Hence, I Hardcoded it!
-		if(playerName == "BMoney")
-		{
-			cb.setImage(sprites[0]);
-		}
-		else if(playerName == "Reyles")
-		{
-			cb.setImage(sprites[1]);
-		}
-		else if(playerName == "SaintKW")
-		{
-			cb.setImage(sprites[2]);
-		}
-		
-		cb.setText(message);
-		cb.fadeInBlock();
-
-		//Reset
-		tempChatBlock.transform.SetParent(scrollableChat.transform);
-		tempChatBlock.GetComponent<RectTransform>().localScale = Vector3.one;
-		tempChatBlock.GetComponent<RectTransform>().anchoredPosition3D = Vector3.zero;
-
-		//Scrollable Chat Size
-		Vector2 tempSize = scrollableChat.GetComponent<RectTransform>().sizeDelta;
-		tempSize.y += gapSize;
-		scrollableChat.GetComponent<RectTransform>().sizeDelta = tempSize;
-		
-		//Add block to the list
-		chatBlocks.Add(tempChatBlock);
-		
-		chatText.interactable = true;
-		numberOfMessages++;
-			
-	}
 	
 }
